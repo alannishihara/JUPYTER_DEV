27d996e511053795b46d1f5fbfabbbbb65361e56
Alan Nishihara <131835263+alannishihara@users.noreply.github.com>
2023-09-12 16:58:29 -0700

    c

--- AN_RANDOM.py
+++ AN_RANDOM.py
@@ -3,5 +3,6 @@ import random
 n1 = 1
 n2 = 20
 
-random_number = random.randint(n1, n2)  # Generates a random integer between n1 and n2 (inclusive)
-print(f"This is a random number between 1 and 20\n{random_number}\n")
+While True:
+    random_number = random.randint(n1, n2)  # Generates a random integer between n1 and n2 (inclusive)
+    print(f"This is a random number between 1 and 20\n{random_number}\n")
