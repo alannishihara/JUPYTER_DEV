9099bee73a8c5aea0484855d37cc883f5372e744
Alan Nishihara <131835263+alannishihara@users.noreply.github.com>
2023-09-13 12:39:37 -0700

    C

--- AN_WHILE_LOOP_V2_3.py
+++
@@ -1,7 +0,0 @@
-while True:
-    name = input('\nPlease enter your name: ')
-    if name == 'Tom':
-        print('\n\tYour access request has been GRANTED, ' + name + '!\n')
-    else:
-        print('\n\tYour access request has been DENIED, ' + name + '!\n')
-    break
